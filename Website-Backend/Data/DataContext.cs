c228540f86ac1f724b74b62c5b487f3ab7893140
vilarj <lamaravillasan@outlook.com>
2023-11-12 08:30:31 -0400

    Conn String | Db context | MySQL packages

---
+++ Website-Backend/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Website_Backend.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Person> people => Set<Person>();
+	}
+}
+
--- Website-Backend/Program.cs
+++ Website-Backend/Program.cs
@@ -1,9 +1,15 @@
 ﻿global using Website_Backend.Services;
 global using Website_Backend.Models;
+global using Microsoft.EntityFrameworkCore;
+global using Website_Backend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
